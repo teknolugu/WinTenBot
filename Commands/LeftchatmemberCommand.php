190efa56c4184c52a675a3a0d5a51d0d058a1ee8
Azhe-kun <azhe403@gmail.com>
2018-08-06 22:09:42 +0700

    fix use namespace

--- Commands/LeftchatmemberCommand.php
+++ Commands/LeftchatmemberCommand.php
@@ -8,7 +8,7 @@
 
 namespace Longman\TelegramBot\Commands\SystemCommands;
 
-use App\Waktu\Waktu;
+use App\Waktu;
 use Longman\TelegramBot\Commands\SystemCommand;
 use Longman\TelegramBot\Entities\InlineKeyboard;
 use Longman\TelegramBot\Request;
