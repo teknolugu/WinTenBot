aa8c0eeba29ca3f1de8bc2d224e78c6200858108
Azhe-kun <azhe403@gmail.com>
2018-08-06 21:33:18 +0700

    app helper baru

--- hook.php
+++ hook.php
@@ -10,7 +10,10 @@
 require_once __DIR__ . '/vendor/autoload.php';
 require_once __DIR__ . '/app/Waktu.php';
 require_once __DIR__ . '/app/Terjemah.php';
+require_once __DIR__ . '/app/FtpUpload.php';
+require_once __DIR__ . '/app/Kata.php';
 require_once __DIR__ . '/config.php';
+
 
 $commands_paths = [
     __DIR__ . '/Commands/',
