1149e04a00aa29290b141dac341e7da25c378a54
Azhe Kun <azhe403@gmail.com>
2018-12-30 11:20:50 +0700

    update /start command

--- Commands/StartCommand.php
+++ Commands/StartCommand.php
@@ -8,6 +8,7 @@
 
 namespace Longman\TelegramBot\Commands\SystemCommands;
 
+use App\Bot;
 use App\Waktu;
 use Longman\TelegramBot\Commands\SystemCommand;
 use Longman\TelegramBot\Request;
@@ -29,19 +30,34 @@ class StartCommand extends SystemCommand
         $time = $message->getDate();
         $time1 = Waktu::jedaNew($time);
 
-        $text = 'Hai, Ada yang bisa saya bantu?';
+        $text = "🤖 <b>WinTen Beta Bot</b> is a debugging bot, group management and other useful equipment." .
+            "\nOfficial Telegram Bot based on <b>WinTen API</b> with Anti-Spam Security!" .
+            "\nFeedback and Request via @WinTenGroup or @TgBotID.\nFor help type /help. " .
+            "\n\nℹ️ /info for more information.";
 
         $pecah = explode(' ', $message->getText(true));
         switch ($pecah[0]) {
             case 'username':
                 return Request::sendDocument([
-                    'chat_id'                  => $chat_id,
-                    'document'                 => 'CgADBQADIgADzjAhVzAzhd8G8GtBAg',
-                    'caption'                  => 'Buka aplikasi Telegram > Settings > Username, lalu isi Username-nya.',
-                    'parse_mode'               => 'HTML',
-                    'reply_to_message_id'      => $mssg_id,
+                    'chat_id' => $chat_id,
+                    'document' => 'CgADBQADIgADzjAhVzAzhd8G8GtBAg',
+                    'caption' => 'Buka aplikasi Telegram > Settings > Username, lalu isi Username-nya.',
+                    'parse_mode' => 'HTML',
+                    'reply_to_message_id' => $mssg_id,
                     'disable_web_page_preview' => true
                 ]);
+                break;
+
+            case 'eula':
+                $text = Bot::getTermsUse('eula');
+                if (isBeta) {
+                    $text = str_replace("WinTen Bot", bot_name, $text);
+                }
+                break;
+
+            case 'opensource':
+//                $text = bot_name . " adalah Open Source, kamu bebas menggunakan dan memodifikasi kode sumber bot. Pull Request sangat kami harapkan";
+                $text = bot_name . " adalah Open Source";
                 break;
 
             case '1':
@@ -53,10 +69,10 @@ class StartCommand extends SystemCommand
         $time = "\n\n ⏱ " . $time1 . ' | ⏳ ' . $time2;
 
         $data = [
-            'chat_id'             => $chat_id,
-            'text'                => $text . $time,
+            'chat_id' => $chat_id,
+            'text' => $text . $time,
             'reply_to_message_id' => $mssg_id,
-            'parse_mode'          => 'HTML'
+            'parse_mode' => 'HTML'
         ];
 
         return Request::sendMessage($data);
---
+++ app/Bot.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: Azhe
+ * Date: 12/16/2018
+ * Time: 3:40 PM
+ */
+
+namespace App;
+
+
+class Bot
+{
+    /**
+     * @param $data
+     * @return false|string
+     */
+    public static function getTermsUse($data)
+    {
+        $text = botData . '/' . $data . ".html";
+        $data = file_get_contents($text);
+        return $data;
+    }
+
+    public static function setTermsUse($data)
+    {
+        $text = botData . '/term-use.html';
+        $data = file_put_contents($text, $data);
+        return $data;
+    }
+
+}
