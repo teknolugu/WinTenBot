f723ff3f395656ae10a56ebeff23fc4954010052
Azhe Kun <azhe403@gmail.com>
2019-02-14 23:30:32 +0700

    add example buttons

---
+++ src/Buttons/Test.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Azhe
+ * Date: 2/9/2019
+ * Time: 9:15 PM
+ */
+
+$urlStart = 'https://t.me/TgBotID';
+
+define('BTN_EXAMPLE', [
+	['text' => 'Tombol 1', 'url' => urlStart],
+	['text' => 'Tombol 2', 'url' => $urlStart],
+	['text' => 'Tombol 3', 'url' => $urlStart],
+]);
