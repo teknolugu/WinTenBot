1cb51a916edb24e51f56962662266818cd1cfc6d
Azhe Kun <azhe403@gmail.com>
2019-02-15 00:11:44 +0700

    update debug command

--- Commands/DbgCommand.php
+++ Commands/DbgCommand.php
@@ -8,47 +8,40 @@
 
 namespace Longman\TelegramBot\Commands\UserCommands;
 
-use src\Utils\Time;
 use Longman\TelegramBot\Commands\UserCommand;
-use Longman\TelegramBot\Request;
+use src\Handlers\MessageHandlers;
 
 class DbgCommand extends UserCommand
 {
-    protected $name = 'dbg';
-    protected $description = 'Get debug message';
-    protected $usage = '<dbg>';
-    protected $version = '1.0.0';
-    /**
-     * Execute command
-     *
-     * @return \Longman\TelegramBot\Entities\ServerResponse
-     * @throws \Longman\TelegramBot\Exception\TelegramException
-     */
-    public function execute()
-    {
-        $message = $this->getMessage();
-        $chat_id = $message->getChat()->getId();
-        $repMssg = $message->getReplyToMessage();
-        $time = $message->getDate();
-
-        if ($repMssg != null) {
-
-            $text = "<b>Debug</b>\n" .
-                "<code>" .
-                json_encode($repMssg->getRawData(),
-                    JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES) .
-                "</code>";
-        } else {
-            $text = "Reply yang akan di debug";
-        }
+	protected $name = 'dbg';
+	protected $description = 'Get debug message';
+	protected $usage = '<dbg>';
+	protected $version = '1.0.0';
 	
-	    $time = Time::jeda($time);
-        $data = [
-            'chat_id' => $chat_id,
-            'text' => $text . $time,
-            'parse_mode' => 'HTML'
-        ];
-
-        return Request::sendMessage($data);
-    }
+	/**
+	 * Execute command
+	 *
+	 * @return \Longman\TelegramBot\Entities\ServerResponse
+	 * @throws \Longman\TelegramBot\Exception\TelegramException
+	 */
+	public function execute()
+	{
+		$message = $this->getMessage();
+		$mHandler = new MessageHandlers($message);
+		$repMssg = $message->getReplyToMessage();
+		
+		$mHandler->sendText('Loading JSON data..');
+		if ($repMssg != null) {
+			$dbg = $repMssg->getRawData();
+		} else {
+			$dbg = $message->getRawData();
+		}
+		
+		$text = "<b>Debug messages</b>\n" .
+			'<code>' .
+			json_encode($dbg, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES) .
+			'</code>';
+		
+		return $mHandler->editText($text);
+	}
 }
