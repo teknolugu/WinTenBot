2ffa08e376284480095fb97413defecea24d770b
Azhe Kun <azhe403@gmail.com>
2019-03-10 13:18:03 +0700

    fix cek /resi

--- src/Model/Resi.php
+++ src/Model/Resi.php
@@ -8,16 +8,29 @@
 
 namespace src\Model;
 
+use GuzzleHttp\Client;
+
 class Resi
 {
-	/**
-	 * @param $kurir
-	 * @param $resi
-	 * @return string
-	 */
-	public static function cekResi($kurir, $resi)
-	{
-		$url = new_api . "/resi?kurir=$kurir&resi=$resi";
-		return file_get_contents($url);
-	}
+    /**
+     * @param $kurir
+     * @param $resi
+     * @return string
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public static function cekResi($kurir, $resi)
+    {
+//        $url = new_api . "/resi?kurir=$kurir&resi=$resi";
+//        return file_get_contents($url);
+
+        $client = new Client(['base_uri' => new_api]);
+        $response = $client->request('GET', '/resi', [
+            'query' => [
+                'kurir' => $kurir,
+                'resi' => $resi
+            ]
+        ]);
+
+        return $response->getBody();
+    }
 }
