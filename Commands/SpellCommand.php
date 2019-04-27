e84d6071258ffba6245b2e0a81ec323993e2d3a2
Azhe Kun <azhe403@gmail.com>
2019-04-28 00:40:40 +0700

    fix spelling

--- Commands/SpellCommand.php
+++ Commands/SpellCommand.php
@@ -9,51 +9,63 @@
 namespace Longman\TelegramBot\Commands\UserCommands;
 
 use Longman\TelegramBot\Commands\UserCommand;
-use src\Handlers\MessageHandlers;
+use Longman\TelegramBot\Entities\ServerResponse;
+use Longman\TelegramBot\Exception\TelegramException;
+use src\Handlers\ChatHandler;
 use src\Model\Group;
 use src\Model\Spell;
 
 class SpellCommand extends UserCommand
 {
-	protected $name = 'spell';
-	protected $description = 'Fix typo into corrected message';
-	protected $usage = '/spell';
-	protected $version = '1.0.0';
-	
-	/**
-	 * @return void
-	 * @throws \Longman\TelegramBot\Exception\TelegramException
-	 */
-	public function execute()
-	{
-		$message = $this->getMessage();
-		$mHandler = new MessageHandlers($message);
-		
-		$repMssg = $message->getReplyToMessage();
-		$data = explode(' ', $message->getText(true));
-		
-		if ($repMssg != '') {
-			$typoMssg = $repMssg->getText();
-			$mHandler->deleteMessage();
-			$mHandler->sendText('Initializing..');
-			$typoMssg = Spell::spellText($typoMssg);
-			$mssg_id = $repMssg->getMessageId();
-			$mHandler->editText("✅ Mungkin yang di maksud adalah:\n" . $typoMssg, $mssg_id);
-		} elseif (count($data) == 2) {
-			$isSudoer = Group::isSudoer($message->getFrom()->getId());
-			if ($isSudoer) {
-				$datas = [
-					'typo'    => $data[0],
-					'fix'     => $data[1],
-					'chat_id' => $message->getChat()->getId(),
-					'user_id' => $message->getFrom()->getId(),
-				];
-				
-				$result = Spell::addSpell($datas);
-				$mHandler->sendText("Ya \n" . $result);
-			}
-		} else {
-			$mHandler->sendText('ℹ <i>Reply</i> pesan yang mau Spell');
-		}
-	}
+    protected $name = 'spell';
+    protected $description = 'Fix typo into corrected message';
+    protected $usage = '/spell';
+    protected $version = '1.0.0';
+
+    /**
+     * @return ServerResponse
+     * @throws TelegramException
+     */
+    public function execute()
+    {
+        $message = $this->getMessage();
+        $chatHandler = new ChatHandler($message);
+
+        $repMssg = $message->getReplyToMessage();
+        $data = explode(' ', $message->getText(true));
+
+        if ($repMssg != '') {
+            $needSpell = $repMssg->getText();
+//			$chatHandler->deleteMessage();
+            $chatHandler->sendText('🔄 Spelling..');
+            $fixedMssg = Spell::spellText($needSpell);
+            $mssg_id = $repMssg->getMessageId();
+            $r = $chatHandler->editText("✅ <i>Mungkin</i> yang di maksud adalah:\n" .
+                "<code>".$fixedMssg."</code>",'-1');
+        } elseif (count($data) == 2) {
+            $r = $chatHandler->sendText("🤔 Checking permission",'-1');
+            $isSudoer = Group::isSudoer($message->getFrom()->getId());
+            if ($isSudoer) {
+                $datas = [
+                    'typo' => $data[0],
+                    'fix' => $data[1],
+                    'chat_id' => $message->getChat()->getId(),
+                    'user_id' => $message->getFrom()->getId(),
+                ];
+
+                $r = $chatHandler->editText("🏗 Adding spell",'-1');
+                $result = Spell::addSpell($datas);
+                if($result->rowCount() > 0){
+                    $text = "✅ Spelling berhasil di tambahkan";
+                }else{
+                    $text = "⚠ Spelling sudah ada atau tidak dapat di perbarui";
+                }
+                $r = $chatHandler->editText($text);
+            }
+        } else {
+            $r = $chatHandler->sendText('ℹ <i>Reply</i> pesan yang mau Spell');
+        }
+
+        return $r;
+    }
 }
