8539870e33a60dac7aae9bc55705486d499ae408
Azhe-kun <azhe403@gmail.com>
2019-05-23 04:18:11 +0700

    fbanreg must admin

--- Commands/UserCommands/FedBan/FbanregCommand.php
+++ Commands/UserCommands/FedBan/FbanregCommand.php
@@ -13,6 +13,7 @@ use Longman\TelegramBot\Entities\ServerResponse;
 use Longman\TelegramBot\Exception\TelegramException;
 use src\Handlers\ChatHandler;
 use src\Model\Fbans;
+use src\Model\Group;
 
 class FbanregCommand extends UserCommand
 {
@@ -35,34 +36,34 @@ class FbanregCommand extends UserCommand
 		$this->description = "Register to $federation_name";
 		
 		$chatHandler->sendText('🤔 Checking permission..', '-1');
-
-//		if (Group::isAdmin($from_id, $chat_id)) {
-		$chatHandler->editText('🏗 Mempersiapkan..');
-		if (!$message->getChat()->isPrivateChat()) {
-			$reg_fban = [
-				'user_id'       => $from_id,
-				'username'      => $message->getFrom()->getUsername(),
-				'promoted_from' => $chat_id,
-			];
-			
-			$chatHandler->editText("🏗 Sedang meregister $federation_name");
-			$result = Fbans::saveAdminFBans($reg_fban);
-			if ($result->rowCount() > 0) {
-				$chatHandler->editText('✍ Menulis ke cache..');
-				Fbans::writeCacheAdminFbans();
-				$text = "✅ <b>Kamu</b> berhasil register ke $federation_name";
+		
+		if (Group::isAdmin($from_id, $chat_id)) {
+			$chatHandler->editText('🏗 Mempersiapkan..');
+			if (!$message->getChat()->isPrivateChat()) {
+				$reg_fban = [
+					'user_id'       => $from_id,
+					'username'      => $message->getFrom()->getUsername(),
+					'promoted_from' => $chat_id,
+				];
+				
+				$chatHandler->editText("🏗 Sedang meregister $federation_name");
+				$result = Fbans::saveAdminFBans($reg_fban);
+				if ($result->rowCount() > 0) {
+					$chatHandler->editText('✍ Menulis ke cache..');
+					Fbans::writeCacheAdminFbans();
+					$text = "✅ <b>Kamu</b> berhasil register ke $federation_name";
+				} else {
+					$text = "✅ <b>Kamu</b> telah register ke $federation_name";
+				}
+				$text .= '. Terimakasih sudah register. 😊';
 			} else {
-				$text = "✅ <b>Kamu</b> telah register ke $federation_name";
+				$text = "ℹ Registrasi $federation_name hanya via Grup.";
 			}
-			$text .= '. Terimakasih sudah register. 😊';
-		} else {
-			$text = "ℹ Registrasi $federation_name hanya via Grup.";
+			return $chatHandler->editText($text);
 		}
-		return $chatHandler->editText($text);
-//		}
-
-//		$r = $chatHandler->editText("⚠ Kamu harus admin agar bisa register ke $federation_name");
-
-//		return $r;
+		
+		$r = $chatHandler->editText("⚠ Kamu harus admin agar bisa register ke $federation_name");
+		
+		return $r;
 	}
 }
