575ae99d147403e516a9da9991d530ec4b60d121
Azhe Kun <azhe403@gmail.com>
2019-07-26 06:20:29 +0700

    add utils

--- src/Utils/Csv.php
+++ src/Utils/Csv.php
@@ -16,4 +16,67 @@ class Csv
 		fputcsv($f, $data);
 		fclose($f);
 	}
+	
+	/**
+	 * @param $file
+	 * @param $writeTo
+	 * @return false|string
+	 */
+	public static function ReadCsv($file, $writeTo)
+	{
+//		$lines = file($fn); // read file directly as an array of lines
+//		array_pop($lines); // you can remove the last empty line (if required)
+//		$json = json_encode(array_map('str_getcsv', $lines), JSON_NUMERIC_CHECK);
+//		$csv= file_get_contents($fn);
+//		$array = array_map('str_getcsv', explode("\n", $csv));
+//		$json = json_encode($array);
+		
+		$fh = fopen($file, "r");
+//
+//		Setup a PHP array to hold our CSV rows.
+		$csvData = [];
+//
+//		Loop through the rows in our CSV file and add them to
+//		the PHP array that we created above.
+		while (($row = fgetcsv($fh, 0, ",")) !== false) {
+			$csvData[] = $row;
+		}
+//
+//		Finally, encode our array into a JSON string format so that we can print it out.
+		$json = json_encode($csvData, 128);
+
+//		$csv= file_get_contents($file);
+//		$array = array_map('str_getcsv', explode("\n", $csv));
+//		$json = json_encode($array,128);
+//
+		file_put_contents($writeTo, $json);
+		return $json;
+	}
+	
+	public static function ConvertJson($fileName)
+	{
+		if (($handle = fopen($fileName, "r")) !== false) {
+			$csvs = [];
+			while (!feof($handle)) {
+				$csvs[] = fgetcsv($handle);
+			}
+			$datas = [];
+			$column_names = [];
+			foreach ($csvs[0] as $single_csv) {
+				$column_names[] = $single_csv;
+			}
+			
+			foreach ($csvs as $key => $csv) {
+				if ($key === 0) {
+					continue;
+				}
+				foreach ($column_names as $column_key => $column_name) {
+					$datas[$key - 1][$column_name] = $csv[$column_key];
+				}
+			}
+			$json = json_encode($datas, 128);
+			fclose($handle);
+		}
+		return $json;
+	}
 }
---
+++ src/Utils/Folder.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace src\Utils;
+
+use Exception;
+use RecursiveDirectoryIterator;
+use RecursiveIteratorIterator;
+
+class Folder
+{
+	/**
+	 * @param $dir
+	 * @return bool
+	 */
+	public static function deleteDir($dir)
+	{
+		try {
+//		$dir = 'samples' . DIRECTORY_SEPARATOR . 'sampledirtree';
+			$it = new RecursiveDirectoryIterator($dir, RecursiveDirectoryIterator::SKIP_DOTS);
+			$files = new RecursiveIteratorIterator($it, RecursiveIteratorIterator::CHILD_FIRST);
+			foreach ($files as $file) {
+				if ($file->isDir()) {
+					rmdir($file->getRealPath());
+				} else {
+					unlink($file->getRealPath());
+				}
+			}
+			rmdir($dir);
+			$delete = true;
+		} catch (Exception $ex) {
+//			$delete = $ex->getMessage();
+			$delete = false;
+		}
+		return $delete;
+	}
+	
+	/**
+	 * @param $fileName
+	 * @return bool
+	 */
+	public static function deleteFile($fileName)
+	{
+		if (file_exists($fileName)) {
+			$deleted = true;
+			unlink($fileName);
+		} else {
+			$deleted = false;
+		}
+		return $deleted;
+	}
+	
+	/**
+	 * @param $dir
+	 * @return bool
+	 */
+	public static function isExist($dir)
+	{
+		return file_exists($dir);
+	}
+}
