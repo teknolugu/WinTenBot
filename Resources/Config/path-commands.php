694a447cd297fee6e60f2298e824c6fe9b2d8e21
Azhe Kun <azhe403@gmail.com>
2019-07-31 21:43:34 +0700

    list path command

---
+++ Resources/Config/path-commands.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+	__DIR__ . '/Commands/SystemCommands',
+	__DIR__ . '/Commands/UserCommands/Additional',
+	__DIR__ . '/Commands/UserCommands/Bot',
+	__DIR__ . '/Commands/UserCommands/CekResi',
+	__DIR__ . '/Commands/UserCommands/Chat',
+	__DIR__ . '/Commands/UserCommands/FedBan',
+	__DIR__ . '/Commands/UserCommands/GitTools',
+	__DIR__ . '/Commands/UserCommands/Group',
+	__DIR__ . '/Commands/UserCommands/Labs',
+	__DIR__ . '/Commands/UserCommands/Member',
+	__DIR__ . '/Commands/UserCommands/Security',
+	__DIR__ . '/Commands/UserCommands/Sudoer',
+	__DIR__ . '/Commands/UserCommands/Tagging',
+	__DIR__ . '/Commands/UserCommands/Texting',
+];
